3eb9c743e08eec043ab2e711ae73b47ab527bedd
MilianoJunior <jrmfilho37@gmail.com>
2024-12-02 14:32:47 -0300

    colocar online

--- main.py
+++ main.py
@@ -126,7 +126,8 @@ def main():
         except Exception as e:
             st.error(f"Erro: {e}")
 
-
-if __name__ == '__main__':
-    main()
+main()
+#
+# if __name__ == '__main__':
+#     main()
 
